4bbfd84896a127a09a8d97fdab9eda1159e4c403
user.name "avinash <avinash>
2026-07-30 15:40:00 +0530

    Initial Commit

---
+++ src/test/java/com/ledger/wellfoundbackend/WellfoundBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ledger.wellfoundbackend;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellfoundBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
